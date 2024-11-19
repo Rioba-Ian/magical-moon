902b51e773c97fc21fb866c3fd9169292f65219c
Rioba-Ian <riobafelix@gmail.com>
2024-11-19 11:57:28 +0300

    add preact for client side interaction

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
- integrations: [tailwind()],
+ output: "hybrid",
+ integrations: [tailwind(), preact({ devtools: true })],
 });
