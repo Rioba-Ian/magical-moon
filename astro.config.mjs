d63d95861038c4e6af3656f8b7e92ca83521f50a
Rioba-Ian <riobafelix@gmail.com>
2024-11-20 17:59:40 +0300

    add vercel for server side rendering and fix deployment issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
- output: "hybrid",
- integrations: [tailwind(), preact({ devtools: true })],
+  output: "hybrid",
+  integrations: [tailwind(), preact({ devtools: true })],
+  adapter: vercel(),
 });
