f4f3f47170c46a9742c43255ba7d22b58a3c58f7
Rioba-Ian <riobafelix@gmail.com>
2024-12-05 18:21:49 +0300

    mobile styling crew members page

--- src/components/preact/Crew.tsx
+++ src/components/preact/Crew.tsx
@@ -1,50 +1,89 @@
-import { signal, useSignal } from "@preact/signals";
+import {
+ computed,
+ Signal,
+ signal,
+ useComputed,
+ useSignal,
+} from "@preact/signals";
 import crewData from "./crew.json";
 import type { HTMLAttributes } from "preact/compat";
 
 export default function CrewComponent() {
- const selectedCrewTab = signal(1);
+ const selectedCrewTab = useSignal(1);
+ const TabToRender = useComputed(() =>
+  crewData?.filter((tab) => tab.id === selectedCrewTab.value)
+ );
 
- console.log(selectedCrewTab.value);
+ const updateSelectedCrewTab = (newTab: string) => {
+  const newCrewId = crewData?.find((crew) => crew.tab === newTab)?.id;
+
+  if (newCrewId) {
+   selectedCrewTab.value = newCrewId;
+  }
+ };
 
  return (
   <section class={"grid grid-cols-1 md:grid-cols-2"}>
-   <article class={" space-y-8 grid grid-cols-1 grid-rows-6"}>
+   <article
+    class={" space-y-8 grid grid-cols-1 grid-rows-6 text-center md:text-left"}
+   >
     <div class={"row-span-5 place-self-center space-y-4"}>
-     {crewData
-      ?.filter((tab) => tab.active)
-      ?.map((data) => (
-       <>
-        <h2 class={"uppercase text-2xl font-font-serif text-white/50"}>
-         {data.role}
-        </h2>
-        <h3 class={"uppercase text-3xl font-serif text-white"}>{data.tab}</h3>
+     {TabToRender.value?.map((data) => (
+      <div class={"py-4"}>
+       <h2
+        class={
+         "uppercase text-xl font-light md:text-2xl font-font-serif text-white/50"
+        }
+       >
+        {data.role}
+       </h2>
+       <h3
+        class={
+         "uppercase py-4 sm:py-8 text-2xl font-light md:text-3xl  font-serif text-white"
+        }
+       >
+        {data.name}
+       </h3>
 
-        <p
-         class={
-          "py-4 text-accent/90 text-base md:text-lg leading-9 tracking-letterSpacing7"
-         }
-        >
-         {data.description}
-        </p>
-       </>
-      ))}
+       <p
+        class={
+         "pt-8 md:py-4 text-accent/90 text-base md:text-lg leading-7 md:leading-9 tracking-letterSpacing7"
+        }
+       >
+        {data.description}
+       </p>
+      </div>
+     ))}
     </div>
 
     {/* dots for navigation */}
-    <div class={"place-self-end justify-self-start"} id="navigation-crew-tab">
+    <div
+     class={"place-self-center md:place-self-end md:justify-self-start"}
+     id="navigation-crew-tab"
+    >
      <ul class={"flex items-center gap-8"}>
       {crewData?.map((crewTab) => (
        <li>
-        <Button active={crewTab.active} />
+        <Button
+         active={crewTab.id === selectedCrewTab.value}
+         id={crewTab.tab}
+         onClick={(e) => updateSelectedCrewTab(e.currentTarget.id)}
+        />
        </li>
       ))}
      </ul>
     </div>
    </article>
 
-   <article class={"place-self-center"}>
-    {crewData?.map((crewTab) => crewTab.active && <img src={crewTab?.img} />)}
+   <article
+    class={
+     "justify-self-center md:place-self-center my-8 md:my-0 py-8 md:py-0 w-4/5 md:w-full"
+    }
+   >
+    {crewData?.map(
+     (crewTab) =>
+      crewTab.id === selectedCrewTab.value && <img src={crewTab?.img} />
+    )}
    </article>
   </section>
  );
@@ -55,9 +94,12 @@ interface CrewTabButtonProps extends HTMLAttributes<HTMLButtonElement> {
 }
 
 const Button = (props: CrewTabButtonProps) => {
- const { active } = props;
+ const { active, ...rest } = props;
  return (
-  <button class={`${active ? "bg-white" : "bg-white/50"} p-2 rounded-full`}>
+  <button
+   class={`${active ? "bg-white" : "bg-white/50"} p-2 rounded-full`}
+   {...rest}
+  >
    <span class={"sr-only"}></span>
   </button>
  );
